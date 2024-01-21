a727d3d27173f3e4b217de39acca8ef97fcfc509
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2024-01-21 22:57:29 +0530

    added: noOfTasks for filter component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,27 +2,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { showActive, showAll, showCompleted } from "../features/filtersSlice";
 
 export default function Filters() {
+  const tasks = useSelector((state) => state.tasks.tasks);
   const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
+  const noOfActiveTasks = tasks.filter(({ completed }) => !completed).length;
+  const noOfCompletedTasks = tasks.filter(({ completed }) => completed).length;
   return (
     <div className="filters">
       <button
         className={filter.allTasks ? "active" : ""}
         onClick={() => dispatch(showAll())}
       >
-        Show All tasks
+        Show All tasks ({tasks.length})
       </button>
       <button
         className={filter.activeTasks ? "active" : ""}
         onClick={() => dispatch(showActive())}
       >
-        Show Active Tasks
+        Show Active Tasks ({noOfActiveTasks})
       </button>
       <button
         className={filter.completedTasks ? "active" : ""}
         onClick={() => dispatch(showCompleted())}
       >
-        Show Completed Tasks
+        Show Completed Tasks ({noOfCompletedTasks})
       </button>
     </div>
   );

b6a1c29bef1bcec00cfe05b3806d59769e5dc92f
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2024-01-21 22:38:42 +0530

    fix: checked attribute

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,11 @@
 import { useDispatch } from "react-redux";
 import "../componentStyles/Task.css";
 import { deleteTask, updateTaskStatus } from "../features/tasksSlice";
+import EditModal from "./EditModal";
+import { useState } from "react";
 
 export default function Task({ id, name, completed }) {
+  const [editActive, setEditActive] = useState(false);
   const dispatch = useDispatch();
   return (
     <div className="task">
@@ -10,6 +13,7 @@ export default function Task({ id, name, completed }) {
         <input
           id={id}
           type="checkbox"
+          checked={completed ? true : false}
           onChange={(e) =>
             dispatch(updateTaskStatus({ id, completed: e.target.checked }))
           }
@@ -18,11 +22,18 @@ export default function Task({ id, name, completed }) {
       </div>
       <div className="action-btns">
         <button>
-          <img src="icons/edit.svg" alt="edit-task-icon" />
+          <img
+            src="icons/edit.svg"
+            alt="edit-task-icon"
+            onClick={() => setEditActive(true)}
+          />
         </button>
         <button onClick={() => dispatch(deleteTask({ id }))}>
           <img src="icons/delete.svg" alt="delete-task-icon" />
         </button>
+        {editActive && (
+          <EditModal id={id} name={name} setEditActive={setEditActive} />
+        )}
       </div>
     </div>
   );
